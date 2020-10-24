55287869daa4650ace877c8f2bfdad5bb8153ef1
Dushmanthi Fernando <madushanimf@gmail.com>
2020-10-24 22:54:51 +0530

    Add files via upload

---
+++ TaskService/src/main/java/com/dushi/taskService/exception/ResourceNotFoundException.java
@@ -0,0 +1,17 @@
+package com.dushi.taskService.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+    private static final long serialVersionUID = 1L;
+
+    public ResourceNotFoundException(String message){
+        super(message);
+    }
+
+    public ResourceNotFoundException(String message,Throwable throwable){
+        super(message,throwable);
+    }
+
+    public ResourceNotFoundException(Throwable throwable){
+        super(throwable);
+    }
+}
---
+++ TaskService/src/main/java/com/dushi/taskService/service/TaskServiceImpl.java
@@ -0,0 +1,9 @@
+package com.dushi.taskService.service;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TaskServiceImpl implements TaskService {
+
+
+    }
+
